f04acea3919aed50e924dd88b7fb79a2e0e8fde3
Wojciech Ciszewski <wociszewski@gmail.com>
2025-03-31 17:01:01 +0200

    Test ORAM on, ORAM off and turning on in the binsearch benchmark

--- src/bin/binsearch.rs
+++ src/bin/binsearch.rs
@@ -3,7 +3,7 @@ use std::{
     time::Instant,
 };
 
-use rand::{distributions::Standard, rngs::OsRng, Rng};
+use rand::{distributions::Standard, rngs::OsRng, CryptoRng, Rng, RngCore};
 use static_assertions::const_assert;
 
 use oram::{path_oram::LINEAR_TIME_ORAM_CUTOFF, Address, DefaultOram, Oram};
@@ -11,6 +11,33 @@ use oram::{path_oram::LINEAR_TIME_ORAM_CUTOFF, Address, DefaultOram, Oram};
 const ARRAY_SIZE: u64 = 4096;
 
 const_assert!(ARRAY_SIZE >= LINEAR_TIME_ORAM_CUTOFF);
+
+fn benchmark_searches<O: Oram<V = u64>, R: RngCore + CryptoRng>(oram_array: &mut O, rng: &mut R) {
+    for _ in 0..20 {
+        let search_val = rng.gen::<u64>();
+
+        let mut l = 0;
+        let mut r = ARRAY_SIZE - 1;
+        let mut found = None;
+
+        let start = Instant::now();
+
+        // Run for a fixed number of iterations to prevent leakage from the number of iterations and keep execution times consistent
+        for _ in 0..ARRAY_SIZE.next_power_of_two().ilog2() {
+            let mid = (l + r) / 2;
+            let val = oram_array.read(mid, rng).unwrap();
+
+            match val.cmp(&search_val) {
+                Ordering::Less => l = min(mid + 1, r),
+                Ordering::Greater => r = max(mid - 1, l),
+                Ordering::Equal => found = Some(mid),
+            }
+        }
+        let duration = start.elapsed();
+
+        println!("Found {:?} in {:?}", found, duration);
+    }
+}
 
 fn main() {
     let mut rng = OsRng;
@@ -27,29 +54,21 @@ fn main() {
         oram_array.write(i as Address, *value, &mut rng).unwrap();
     }
 
-    // Run for a fixed number of iterations to prevent leakage from the number of iterations and keep execution times consistent
-    let n_iterations = ARRAY_SIZE.next_power_of_two().ilog2();
+    println!("ORAM on:");
 
-    for _ in 0..20 {
-        let search_val = rng.gen::<u64>();
+    benchmark_searches(&mut oram_array, &mut rng);
 
-        let mut l = 0;
-        let mut r = ARRAY_SIZE - 1;
-        let mut found = None;
+    println!("ORAM off:");
 
-        let start = Instant::now();
-        for _ in 0..n_iterations {
-            let mid = (l + r) / 2;
-            let val = oram_array.read(mid, &mut rng).unwrap();
+    oram_array.turn_off().unwrap();
 
-            match val.cmp(&search_val) {
-                Ordering::Less => l = min(mid + 1, r),
-                Ordering::Greater => r = max(mid - 1, l),
-                Ordering::Equal => found = Some(mid),
-            }
-        }
-        let duration = start.elapsed();
+    benchmark_searches(&mut oram_array, &mut rng);
 
-        println!("Found {:?} in {:?}", found, duration);
-    }
+    let start = Instant::now();
+
+    oram_array.turn_on(&mut rng).unwrap();
+
+    let duration = start.elapsed();
+
+    println!("Turned on in {:?}", duration);
 }

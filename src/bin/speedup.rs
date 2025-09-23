09b11ccd55fbed0aa5b7786c928953fce5881079
Wojciech Ciszewski <wociszewski@gmail.com>
2025-09-23 20:50:48 +0200

    Make the speedup test more parameterizable

--- src/bin/speedup.rs
+++ src/bin/speedup.rs
@@ -21,7 +21,7 @@ const ARRAY_SIZE: u64 = 1 << 17;
 
 const_assert!(ARRAY_SIZE >= LINEAR_TIME_ORAM_CUTOFF);
 
-const N_UNIQUE_ADDRESSES: u64 = 100;
+const N_UNIQUE_ADDRESSES: [u64; 1] = [100];
 
 const AVERAGE_N_ACCESSES_PER_ADDR: [u64; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -30,14 +30,15 @@ const N_BENCHMARK_REPETITIONS: u32 = 20;
 fn benchmark_percentages<O: Oram<V = u64>, R: rand::RngCore + rand::CryptoRng>(
     oram_array: &mut O,
     rng: &mut R,
+    n_unique_addresses: u64,
     average_n_accesses_per_addr: u64,
 ) -> Result<BenchmarkResult, oram::OramError> {
-    let mut addresses = (0..N_UNIQUE_ADDRESSES)
+    let mut addresses = (0..n_unique_addresses)
         .chain(
-            Uniform::from(0..N_UNIQUE_ADDRESSES)
+            Uniform::from(0..n_unique_addresses)
                 .sample_iter(&mut *rng)
                 .take(
-                    ((average_n_accesses_per_addr - 1) * N_UNIQUE_ADDRESSES)
+                    ((average_n_accesses_per_addr - 1) * n_unique_addresses)
                         .try_into()
                         .unwrap(),
                 ),
@@ -101,33 +102,41 @@ fn main() {
 
     println!("Averaging over {N_BENCHMARK_REPETITIONS} repetitions");
 
-    for average_n_accesses_per_addr in AVERAGE_N_ACCESSES_PER_ADDR {
-        let results = iter::repeat_with(|| {
-            benchmark_percentages(&mut oram_array, &mut rng, average_n_accesses_per_addr).unwrap()
-        })
-        .take(N_BENCHMARK_REPETITIONS.try_into().unwrap())
-        .collect::<Vec<_>>();
+    for n_unique_addresses in N_UNIQUE_ADDRESSES {
+        for average_n_accesses_per_addr in AVERAGE_N_ACCESSES_PER_ADDR {
+            let results = iter::repeat_with(|| {
+                benchmark_percentages(
+                    &mut oram_array,
+                    &mut rng,
+                    n_unique_addresses,
+                    average_n_accesses_per_addr,
+                )
+                .unwrap()
+            })
+            .take(N_BENCHMARK_REPETITIONS.try_into().unwrap())
+            .collect::<Vec<_>>();
 
-        let BenchmarkStats {
-            access_on_duration,
-            off_total_duration,
-            access_off_duration,
-            turn_on_duration,
-        } = benchmark_stats(&results);
+            let BenchmarkStats {
+                access_on_duration,
+                off_total_duration,
+                access_off_duration,
+                turn_on_duration,
+            } = benchmark_stats(&results);
 
-        println!(
-            "ON mode: accessed {N_UNIQUE_ADDRESSES} addresses with {average_n_accesses_per_addr} accesses per addr on average in {:?} +- {:?}",
-            access_on_duration.mean, access_on_duration.stddev
-        );
-        println!(
-            "OFF mode: accessed {N_UNIQUE_ADDRESSES} addresses with {average_n_accesses_per_addr} accesses per addr \
-            on average in {:?} +- {:?} (online accesses took {:?} +- {:?}, turning on took {:?} +- {:?})",
-            off_total_duration.mean,
-            off_total_duration.stddev,
-            access_off_duration.mean,
-            access_off_duration.stddev,
-            turn_on_duration.mean,
-            turn_on_duration.stddev
-        );
+            println!(
+                "ON mode: accessed {n_unique_addresses} addresses with {average_n_accesses_per_addr} accesses per addr on average in {:?} +- {:?}",
+                access_on_duration.mean, access_on_duration.stddev
+            );
+            println!(
+                "OFF mode: accessed {n_unique_addresses} addresses with {average_n_accesses_per_addr} accesses per addr \
+                on average in {:?} +- {:?} (online accesses took {:?} +- {:?}, turning on took {:?} +- {:?})",
+                off_total_duration.mean,
+                off_total_duration.stddev,
+                access_off_duration.mean,
+                access_off_duration.stddev,
+                turn_on_duration.mean,
+                turn_on_duration.stddev
+            );
+        }
     }
 }

3b21e59223d2cc9f0f4719f9a89542ba122c2187
Wojciech Ciszewski <wociszewski@gmail.com>
2025-09-24 23:01:22 +0200

    Speedup vs array size

--- src/bin/speedup.rs
+++ src/bin/speedup.rs
@@ -4,11 +4,10 @@ use log::LevelFilter;
 use rand::{
     distr::slice::Choose,
     prelude::Distribution,
-    random, rng,
+    rng,
     seq::{IteratorRandom, SliceRandom},
 };
 use simplelog::SimpleLogger;
-use static_assertions::const_assert;
 
 use oram::{
     BlockValue, Oram,
@@ -22,23 +21,43 @@ use oram::DefaultOram;
 #[cfg(feature = "bypass_oram")]
 use oram::not_really_oram::NotReallyOram;
 
-const ARRAY_SIZE: u64 = 1 << 17;
-
-const_assert!(ARRAY_SIZE >= LINEAR_TIME_ORAM_CUTOFF);
+const ARRAY_SIZE: [u64; 20] = [
+    1 << 6,
+    1 << 7,
+    1 << 8,
+    1 << 9,
+    1 << 10,
+    1 << 11,
+    1 << 12,
+    1 << 13,
+    1 << 14,
+    1 << 15,
+    1 << 16,
+    1 << 17,
+    1 << 18,
+    1 << 19,
+    1 << 20,
+    1 << 21,
+    1 << 22,
+    1 << 23,
+    1 << 24,
+    1 << 25,
+];
 
 const N_UNIQUE_ADDRESSES: [u64; 1] = [100];
 
-const AVERAGE_N_ACCESSES_PER_ADDR: [u64; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const AVERAGE_N_ACCESSES_PER_ADDR: [u64; 1] = [1];
 
-const N_BENCHMARK_REPETITIONS: u32 = 20;
+const N_BENCHMARK_REPETITIONS: u32 = 100;
 
 fn gen_addresses<R: rand::RngCore + rand::CryptoRng>(
     rng: &mut R,
+    array_size: u64,
     n_unique_addresses: u64,
     average_n_accesses_per_addr: u64,
 ) -> Vec<u64> {
     let unique_addresses =
-        (0..ARRAY_SIZE).choose_multiple(rng, n_unique_addresses.try_into().unwrap());
+        (0..array_size).choose_multiple(rng, n_unique_addresses.try_into().unwrap());
     let mut addresses = unique_addresses
         .iter()
         .copied()
@@ -61,10 +80,16 @@ fn gen_addresses<R: rand::RngCore + rand::CryptoRng>(
 fn benchmark_percentages<O: Oram, R: rand::RngCore + rand::CryptoRng>(
     oram_array: &mut O,
     rng: &mut R,
+    array_size: u64,
     n_unique_addresses: u64,
     average_n_accesses_per_addr: u64,
 ) -> Result<BenchmarkResult, oram::OramError> {
-    let on_addresses = gen_addresses(rng, n_unique_addresses, average_n_accesses_per_addr);
+    let on_addresses = gen_addresses(
+        rng,
+        array_size,
+        n_unique_addresses,
+        average_n_accesses_per_addr,
+    );
 
     let access_on_start = Instant::now();
 
@@ -74,7 +99,12 @@ fn benchmark_percentages<O: Oram, R: rand::RngCore + rand::CryptoRng>(
 
     let access_on_duration = access_on_start.elapsed();
 
-    let off_addresses = gen_addresses(rng, n_unique_addresses, average_n_accesses_per_addr);
+    let off_addresses = gen_addresses(
+        rng,
+        array_size,
+        n_unique_addresses,
+        average_n_accesses_per_addr,
+    );
 
     oram_array.turn_off()?;
 
@@ -100,75 +130,89 @@ fn benchmark_percentages<O: Oram, R: rand::RngCore + rand::CryptoRng>(
 }
 
 fn main() {
-    SimpleLogger::init(LevelFilter::Trace, simplelog::Config::default()).unwrap();
+    SimpleLogger::init(LevelFilter::Warn, simplelog::Config::default()).unwrap();
 
     let mut rng = rng();
 
-    let start = Instant::now();
+    for array_size in ARRAY_SIZE {
+        assert!(array_size >= LINEAR_TIME_ORAM_CUTOFF);
 
-    let mut oram_array = {
-        #[cfg(not(feature = "bypass_oram"))]
-        {
-            DefaultOram::<BlockValue<64>>::new(ARRAY_SIZE, &mut rng).unwrap()
-        }
+        // let start = Instant::now();
 
-        #[cfg(feature = "bypass_oram")]
-        {
-            NotReallyOram::<BlockValue<64>>::new(ARRAY_SIZE).unwrap()
-        }
-    };
+        let mut oram_array = {
+            #[cfg(not(feature = "bypass_oram"))]
+            {
+                DefaultOram::<BlockValue<64>>::new(array_size, &mut rng).unwrap()
+            }
 
-    let duration = start.elapsed();
+            #[cfg(feature = "bypass_oram")]
+            {
+                NotReallyOram::<BlockValue<64>>::new(array_size).unwrap()
+            }
+        };
 
-    println!("Initialized ORAM in {:?}", duration);
+        // let duration = start.elapsed();
 
-    let start = Instant::now();
+        // println!("Initialized ORAM in {:?}", duration);
 
-    for i in 0..ARRAY_SIZE {
-        oram_array.write(i, random(), &mut rng).unwrap();
-    }
+        // let start = Instant::now();
 
-    let duration = start.elapsed();
+        // for i in 0..array_size {
+        //     oram_array.write(i, random(), &mut rng).unwrap();
+        // }
 
-    println!("Prepared array in {:?}", duration);
+        // let duration = start.elapsed();
 
-    println!("Averaging over {N_BENCHMARK_REPETITIONS} repetitions");
+        // println!("Prepared array in {:?}", duration);
 
-    for n_unique_addresses in N_UNIQUE_ADDRESSES {
-        for average_n_accesses_per_addr in AVERAGE_N_ACCESSES_PER_ADDR {
-            let results = iter::repeat_with(|| {
-                benchmark_percentages(
-                    &mut oram_array,
-                    &mut rng,
-                    n_unique_addresses,
-                    average_n_accesses_per_addr,
-                )
-                .unwrap()
-            })
-            .take(N_BENCHMARK_REPETITIONS.try_into().unwrap())
-            .collect::<Vec<_>>();
+        // println!("Averaging over {N_BENCHMARK_REPETITIONS} repetitions");
 
-            let BenchmarkStats {
-                access_on_duration,
-                off_total_duration,
-                access_off_duration,
-                turn_on_duration,
-            } = benchmark_stats(&results);
+        for n_unique_addresses in N_UNIQUE_ADDRESSES {
+            for average_n_accesses_per_addr in AVERAGE_N_ACCESSES_PER_ADDR {
+                let results = iter::repeat_with(|| {
+                    benchmark_percentages(
+                        &mut oram_array,
+                        &mut rng,
+                        array_size,
+                        n_unique_addresses,
+                        average_n_accesses_per_addr,
+                    )
+                    .unwrap()
+                })
+                .take(N_BENCHMARK_REPETITIONS.try_into().unwrap())
+                .collect::<Vec<_>>();
 
-            println!(
-                "ON mode: accessed {n_unique_addresses} addresses with {average_n_accesses_per_addr} accesses per addr on average in {:?} +- {:?}",
-                access_on_duration.mean, access_on_duration.stddev
-            );
-            println!(
-                "OFF mode: accessed {n_unique_addresses} addresses with {average_n_accesses_per_addr} accesses per addr \
-                on average in {:?} +- {:?} (online accesses took {:?} +- {:?}, turning on took {:?} +- {:?})",
-                off_total_duration.mean,
-                off_total_duration.stddev,
-                access_off_duration.mean,
-                access_off_duration.stddev,
-                turn_on_duration.mean,
-                turn_on_duration.stddev
-            );
+                let BenchmarkStats {
+                    access_on_duration,
+                    off_total_duration,
+                    access_off_duration,
+                    turn_on_duration,
+                } = benchmark_stats(&results);
+
+                // println!(
+                //     "ON mode: accessed {n_unique_addresses} addresses with {average_n_accesses_per_addr} accesses per addr on average in {:?} +- {:?}",
+                //     access_on_duration.mean, access_on_duration.stddev
+                // );
+                // println!(
+                //     "OFF mode: accessed {n_unique_addresses} addresses with {average_n_accesses_per_addr} accesses per addr \
+                //     on average in {:?} +- {:?} (online accesses took {:?} +- {:?}, turning on took {:?} +- {:?})",
+                //     off_total_duration.mean,
+                //     off_total_duration.stddev,
+                //     access_off_duration.mean,
+                //     access_off_duration.stddev,
+                //     turn_on_duration.mean,
+                //     turn_on_duration.stddev
+                // );
+                for stat in [
+                    access_on_duration,
+                    off_total_duration,
+                    access_off_duration,
+                    turn_on_duration,
+                ] {
+                    print!("{}; {}; ", stat.mean.as_nanos(), stat.stddev.as_nanos());
+                }
+                println!();
+            }
         }
     }
 }
